5ce19818bc968d98f09bf277e0a30ae4675a87ab
DarVy21 <vyal.darya@mail.ru>
2020-05-02 17:17:02 +0300

    add solution for subtask3

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -2,8 +2,62 @@ package subtask3
 
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var countStr=0
+        var countNeg=0
+        var res:Int=1
+        itemsFromArray.forEach {it->
+            if (it is String ){
+                countStr++
+            }
+            if ((it is Int)&& it<0){
+                countNeg++
+            }
+        }
+        if (countStr==itemsFromArray.size){
+            return 0
+        }
+        if (itemsFromArray.size-countStr<=numberOfItems){
+            itemsFromArray.forEach {
+                it->if (it is Int){
+                res*=it
+            }
+            }
+            return res
+        }
+        if (countNeg==1){
+            itemsFromArray.sort()
+           for (i in itemsFromArray.size-1 downTo itemsFromArray.size-numberOfItems){
+               res*=itemsFromArray[i] as Int
+           }
+            return res
+        }
+        else if (countNeg>1){
+
+            var tempNeg=1
+            var tempPos=1
+            var tempNPos=1
+
+            itemsFromArray.sort()
+
+                tempNeg= itemsFromArray[0] as Int * itemsFromArray[1] as Int
+                /*for (i in itemsFromArray.size-1  downTo  itemsFromArray.size-2){
+                    tempPos*=itemsFromArray[i] as Int
+                }*/
+            for (i in itemsFromArray.size-1 downTo  itemsFromArray.size-(numberOfItems-2)){
+                tempNeg*=itemsFromArray[i] as Int
+            }
+                for (i in itemsFromArray.size-1 downTo  itemsFromArray.size-numberOfItems){
+                    tempNPos*=itemsFromArray[i] as Int
+                }
+            itemsFromArray.forEach { println(it) }
+            println(tempNeg)
+            println(tempNPos)
+            println(tempPos)
+                  return  if (tempNeg>tempNPos) tempNeg else tempNPos
+            }
+
+       return 0
     }
 }
+
