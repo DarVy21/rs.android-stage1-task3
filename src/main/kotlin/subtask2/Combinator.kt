bc2fe8e3de352ed2a33bd0f766d8fbc796049f13
DarVy21 <vyal.darya@mail.ru>
2020-05-03 21:55:54 +0300

    add solution for subtask2

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -2,8 +2,26 @@ package subtask2
 
 class Combinator {
 
-    // TODO: Complete the following function
+
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+
+        for(x in 0 .. (n/2 )){
+            if(m*fact(x)*fact(n-x) == fact(n))
+                return x
+        }
+
+        return null
     }
+
+     fun fact (n : Int) : Int {
+        if((n == 0) || (n == 1)) return 1
+        var f = 1
+        for(k in 1 ..  n){
+            f*=k
+        }
+        return f
+    }
+
 }
